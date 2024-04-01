fa5b5659a94be83976afcd49107ac39712483402
Alexander <abeaumier@deloitte.com>
2024-04-01 11:19:21 -0700

    "added map() iteration"

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // Import data and WeatherCard here
-
+import cities  from './data';
+import WeatherCard from './components/WeatherCard';
 
 function App() {
     return (
@@ -8,7 +9,10 @@ function App() {
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
-                {/* Render components here */}
+                {/* Map through the cities array and render a WeatherCard component for each city */}
+                {cities.map((city, index) => {
+                    return <WeatherCard key = {index} city = {city.city} temperature = {city.temperature} forecast = {city.forecast} />
+                })}
                 
 
 
